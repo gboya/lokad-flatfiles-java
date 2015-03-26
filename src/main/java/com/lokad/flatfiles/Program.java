c7bbab0fb9b040eff1688ac9e024212e38787c5b
Gabriel Boya <gabrielboya@gmail.com>
2015-03-26 15:48:07 +0100

    Add command line switch to validate an existing RawFlatFile

--- src/main/java/com/lokad/flatfiles/Program.java
+++ src/main/java/com/lokad/flatfiles/Program.java
@@ -1,5 +1,6 @@
 package com.lokad.flatfiles;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,7 +13,35 @@ public final class Program
 {
 	static public void main(String[] args) throws IOException
 	{
+		if(args.length == 0) {
+			System.out.println("=== USAGE : ===");
+			System.out.println();
+			System.out.println("1. Creating a RawFlatFile from a TSV/CSV file : ");
+			System.out.println("java -jar lokad-flat-files.jar InputFile.tsv OutputFile.rff");
+			System.out.println();
+			System.out.println("2. Validating an existing RawFlatFile : ");
+			System.out.println("java -jar lokad-flat-files.jar -check RawFlatFile.rff");
+			
+			return;
+		}
 		
+		// Case 2 : Checking consistency of an existing RawFlatFile		
+		if (args[0].equals("-check")) {
+			String rffPath = args[1];
+			try (FileInputStream file = new FileInputStream(rffPath);
+					// Using a BufferedInputStream here improves performance dramatically
+					DataInputStream in = new DataInputStream(new BufferedInputStream(file))) {
+				RawFlatFile rff = RawFlatFileSerialization.ReadRawFlatFile(in);
+				rff.ThrowIfInconsistent();
+				System.out.println("File : " + rffPath + " is well formed.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+		
+		
+		// Case 1 : Converting a TSV file to a RawFlatFile
 		String inputFile = args[0];
 		String outputFile = args[1];
 
